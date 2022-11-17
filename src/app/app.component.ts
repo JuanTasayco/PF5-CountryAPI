89919dc94899a42a7aae6e9451c53144d3b66e32
JuanTasayco <juantasayco266@gmail.com>
2022-11-16 19:15:37 -0500

    v0.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import * as mapboxgl from "mapbox-gl";
 import { environment } from 'src/environments/environment';
+import { gsap } from "gsap"
+import { ScrollTrigger } from 'gsap/all';
+import { TooltipModule } from 'primeng/tooltip';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: []
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
+
+
+  @ViewChild("portada") portada!: ElementRef;
+  @ViewChild("a") block!: ElementRef;
+
   ngOnInit(): void {
     (mapboxgl as any).accessToken = environment.mapboxKey;
+    gsap.registerPlugin(ScrollTrigger);
   }
+
+
+  ngAfterViewInit(): void {
+    /* gsap.defaults({ duration: 2, ease: "none" });
+
+    gsap.set(this.block.nativeElement, {
+      xPercent: 0
+    })
+
+    
+        const tl = gsap.timeline();
+    
+            tl.to(this.block.nativeElement, {
+              xPercent: -100,
+            })
+        
+            ScrollTrigger.create({
+              animation: tl,
+              trigger: this.portada.nativeElement,
+              markers: true,
+              start: "top top",
+              end: "+=2000",
+              scrub: true,
+              pin: true
+            }) */
+
+  }
+
   title = '06-Filters';
 }
